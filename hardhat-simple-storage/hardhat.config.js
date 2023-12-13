7be18f62c5c34f31875e37a5103e64bc954bd7c4
arunanksharan <arunank.sharan@gmail.com>
2023-12-13 23:18:29 +0530

    1. hardhat-gas-reporter integrated with coinmarketcap api + 2. solidity coverage integrated for test coverage

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -2,10 +2,14 @@ require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 require("@nomicfoundation/hardhat-verify")
 require("./tasks/block-number")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: "hardhat",
@@ -24,5 +28,14 @@ module.exports = {
     // Your API key for Etherscan
     // Obtain one at https://etherscan.io/
     apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "ETH",
+    gasPrice: 21,
   },
 }
