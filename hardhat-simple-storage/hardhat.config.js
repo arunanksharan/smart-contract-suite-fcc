9d000e421b9ccbcd50dccc965b9b1e0cff2cf2ec
arunanksharan <arunank.sharan@gmail.com>
2023-12-13 20:22:10 +0530

    Added programmatic contract verification on etherscan using run, hardhat-verify plugin and etherscan api key

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -1,6 +1,27 @@
-require('@nomicfoundation/hardhat-toolbox');
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("@nomicfoundation/hardhat-verify")
 
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: '0.8.7',
-};
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
+  solidity: "0.8.7",
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY,
+  },
+}
--- hardhat-simple-storage/scripts/deploy.js
+++ hardhat-simple-storage/scripts/deploy.js
@@ -1,5 +1,5 @@
 // imports
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 // define async main
 async function main() {
@@ -8,6 +8,30 @@ async function main() {
   const simpleStorage = await SimpleStorageFactory.deploy()
   // await simpleStorage.deployed()
   console.log(`Deployed Contract to: ${simpleStorage.target}`)
+  // What happens when we deploy to hardhat network - useless since local network
+  console.log(network.config)
+  if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+    await simpleStorage.deploymentTransaction().wait(6)
+    await verify(simpleStorage.target, [])
+  }
+
+  // Interacting with the deployed contract
+}
+
+async function verify(contractAddress, args) {
+  try {
+    console.log("Verifying contract...")
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  } catch (error) {
+    if (error.message.includes("Contract source code already verified")) {
+      console.log("Contract source code already verified")
+    } else {
+      console.error(error)
+    }
+  }
 }
 
 // call main function
